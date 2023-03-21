2ea7aeadd10ed750c8880a80350981b050b186a8
VanessaSayuri <sayurivanessa@gmail.com>
2023-03-21 11:13:41 -0300

    first commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,121 @@
+import loginPage from '../support/pages/login'
+import shaversPage from '../support/pages/shavers'
+
+
+
+describe('login', ()=>{
+
+
+    context('quando submeto o formulário', () =>{
+
+        
+
+        it('deve logar com sucesso', ()=>{
+            const user = {
+                name: 'Vanessa',
+                email: 'vanessa@yahoo.com',
+                password: 'pwd123'
+            }
+
+
+            loginPage.submit(user.email,user.password)
+
+            shaversPage.header.userShouldBeLoggedIn(user.name)
+
+        })
+
+
+        it('não deve logar com senha incorreta', ()=>{
+            const user = {
+                name: 'Vanessa',
+                email: 'vanessa@yahoo.com',
+                password: '123456'
+            }
+
+            
+            loginPage.submit(user.email,user.password)
+
+            const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais.'
+            
+            loginPage.noticeShouldBe(message)
+
+        })
+
+
+        it('não deve logar com email não cadastrado', ()=>{
+            const user = {
+                name: 'Vanessa',
+                email: 'vanessa@404.com',
+                password: '123456'
+            }
+
+            loginPage.submit(user.email,user.password)
+
+
+            const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais.'
+            
+            loginPage.noticeShouldBe(message)
+
+        })
+
+        it('campos obrigatorios', ()=>{
+            loginPage.submit()
+
+
+
+            loginPage.requiredFields('E-mail é obrigatório', 'Senha é obrigatória')
+
+        })
+
+        
+    })
+
+
+
+
+    context('senha muito curta', ()=>{
+
+        const passwords = [
+            '1',
+            '12',
+            '123',
+            '1234',
+            '12345'
+        ]
+
+        passwords.forEach((p)=>{
+            it(`não deve logar com a senha: ${p} `, ()=>{
+                loginPage.submit('vanessa@teste.com.br', p)
+
+                loginPage.alertShouldBe('Pelo menos 6 caracteres')
+
+            })
+        })
+    })
+
+
+    context('email no formato incorreto', ()=>{
+
+        const emails = [
+            'vanessa&gmail.com',
+            'vanessa.com.br',
+            '@gmail.com',
+            '@',
+            'vanessa@',
+            '12312',
+            '@#!$%%%',
+            'xpto123'
+        ]
+
+
+        emails.forEach((e)=>{
+            it(`não deve logar com o email: ${e} `, ()=>{
+                loginPage.submit(e, 'pwd123')
+
+                loginPage.alertShouldBe('Informe um email válido')
+
+            })
+        })
+    })
+
+})
